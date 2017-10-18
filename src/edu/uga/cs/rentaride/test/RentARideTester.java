5eb1b3889d31b4e0b3b1a2a285d47295c1d77f88
ShepOgden <shepvogden@gmail.com>
2017-10-18 19:02:18 -0400

    test started

--- src/edu/uga/cs/rentaride/test/RentARideTester.java
+++ src/edu/uga/cs/rentaride/test/RentARideTester.java
@@ -1,9 +1,35 @@
 package edu.uga.cs.rentaride.test;
 
+import java.sql.Connection;
+import java.util.Iterator;
+import java.util.List;
+
 public class RentARideTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Connection  conn = null;
+        ObjectLayer objectLayer = null;
+        PersistenceLayer persistence = null;
+
+        // get a database connection                                                                     
+        try {
+            conn = DbUtils.connect();
+        }
+        catch(Exception seq) {
+            System.err.println( "ReadTest: Unable to obtain a database connection" );
+        }
+
+        if( conn == null ) {
+            System.out.println( "ReadTest: failed to connect to the database" );
+            return;
+        }
+
+        // obtain a reference to the ObjectModel module                                                  
+        objectLayer = new ObjectLayerImpl();
+        // obtain a reference to Persistence module and connect it to the ObjectModel                    
+        persistence = new PersistenceLayerImpl( conn, objectLayer );
+        // connect the ObjectModel module to the Persistence module                                      
+        objectLayer.setPersistence( persistence );
 
 	}
 
