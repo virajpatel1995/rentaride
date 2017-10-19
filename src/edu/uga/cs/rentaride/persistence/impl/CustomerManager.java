caf233fdd5d1aae3747ff3d9b61873bf87697cad
Jambrose777 <Jaa74458@uga.edu>
2017-10-19 17:05:30 -0400

    Fixed where someone moved Administrator Manager code in the worng place
    -_-

--- src/edu/uga/cs/rentaride/persistence/impl/CustomerManager.java
+++ src/edu/uga/cs/rentaride/persistence/impl/CustomerManager.java
@@ -33,7 +33,80 @@ public class CustomerManager {
 	}//constructor
 	
 	public void store(Customer customer) {
-		//TODO
+		String insertCustomerSql = "insert into user ( type, firstName, lastName, userName, password, email, address, createdDate ) values ( 'Administrator', ?, ?, ?, ?, ?, ?, ? )";
+		String updateCustomerSql = "update person  set type = \"Customer\", firstName = ?, lastName = ?, userName = ?, password = ?, email = ?, address = ?, createdDate = ? where id = ?";
+		java.sql.PreparedStatement stmt = null;
+		int inscnt;
+		long userId;
+		
+		try {
+			if(!administrator.isPersistent())
+				stmt = (java.sql.PreparedStatement) conn.prepareStatement(insertAdministratorSql);
+			else
+				stmt = (java.sql.PreparedStatement) conn.prepareStatement(updateAdministratorSql);
+		
+			if(administrator.getFirstName() != null)
+					stmt.setString(1,administrator.getFirstName());
+			else
+					throw new RARException("AdministratorManager.save: can't save an Administrator: FirstName undefined");
+
+			if(administrator.getLastName() != null)
+					stmt.setString(2,administrator.getLastName());
+			else
+					throw new RARException("AdministratorManager.save: can't save an Administrator: LastName undefined");
+
+			if(administrator.getUserName() != null)
+					stmt.setString(3,administrator.getUserName());
+			else
+					throw new RARException("AdministratorManager.save: can't save an Administrator: UserName undefined");
+
+			if(administrator.getPassword() != null)
+					stmt.setString(4,administrator.getPassword());
+			else
+					throw new RARException("AdministratorManager.save: can't save an Administrator: Password undefined");
+
+			if(administrator.getEmail() != null)
+					stmt.setString(5,administrator.getEmail());
+			else
+					throw new RARException("AdministratorManager.save: can't save an Administrator: Email undefined");
+
+			if(administrator.getAddress() != null)
+					stmt.setString(6,administrator.getAddress());
+			else
+					throw new RARException("AdministratorManager.save: can't save an Administrator: Address undefined");
+
+			if(administrator.getCreatedDate() != null)
+					stmt.setString(7,administrator.getCreatedDate().toString());
+			else
+					throw new RARException("AdministratorManager.save: can't save an Administrator: FirstName undefined");
+		
+		}catch (Exception e) {
+
+			if(administrator.isPersistent())
+				stmt.setLong(8,  administrator.getId());
+		
+			inscnt = stmt.executeUpdate();
+			
+			if(!administrator.isPersistent()) {
+				if(inscnt == 1) {
+					String sql = "select last_insert_id()";
+					if(stmt.execute(sql)) {
+						//retrieve the result
+						ResultSet r =stmt.getResultSet();
+						while(r.next()) {
+							userId = r.getLong(1);
+							if(userId > 0)
+								administrator.setId(userId);
+						}//while
+					}//if
+				}//if
+			}else {
+				if(inscnt < 1)
+					throw new RARException("AdministratorManager.save: failed to save an Administrator");
+			}//if else
+			
+		}
+	
 	}//store
 	
 	public List<Customer> restore(Customer customer) throws RARException{
