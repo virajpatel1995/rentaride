2011e7338e494f3089ee517c733ceb893dd1b095
Jambrose777 <Jaa74458@uga.edu>
2017-10-19 18:07:00 -0400

    HourlyPrice Store done

--- src/edu/uga/cs/rentaride/persistence/impl/HourlyPriceManager.java
+++ src/edu/uga/cs/rentaride/persistence/impl/HourlyPriceManager.java
@@ -52,98 +52,45 @@ public class HourlyPriceManager {
 			else
 				stmt = (java.sql.PreparedStatement) conn.prepareStatement(updateHourlyPriceSql);
 		
-			if(hourlyPrice.getMaxHours() < 0)
+			if(hourlyPrice.getMaxHours() > 0)
 					stmt.setInt(1,hourlyPrice.getMaxHours());
 			else
-					throw new RARException("HourlyPriceManager.save: can't save a Customer: FirstName undefined");
+					throw new RARException("HourlyPriceManager.save: can't save a Hourly Price: max hours undefined");
 
-			if(customer.getLastName() != null)
-					stmt.setString(3,customer.getLastName());
+			if(hourlyPrice.getPrice() > 0)
+					stmt.setInt(2,hourlyPrice.getPrice());
 			else
-					throw new RARException("CustomerManager.save: can't save a Customer: LastName undefined");
+					throw new RARException("HourlyPriceManager.save: can't save a HourlyPrice: Price undefined");
 
-			if(customer.getUserName() != null)
-					stmt.setString(4,customer.getUserName());
-			else
-					throw new RARException("CustomerManager.save: can't save a Customer: UserName undefined");
-
-			if(customer.getPassword() != null)
-					stmt.setString(5,customer.getPassword());
-			else
-					throw new RARException("CustomerManager.save: can't save a Customer: Password undefined");
-
-			if(customer.getEmail() != null)
-					stmt.setString(6,customer.getEmail());
-			else
-					throw new RARException("CustomerManager.save: can't save a Customer: Email undefined");
-
-			if(customer.getAddress() != null)
-					stmt.setString(7,customer.getAddress());
-			else
-					throw new RARException("CustomerManager.save: can't save a Customer: Address undefined");
-
-			if(customer.getCreatedDate() != null)
-					stmt.setDate(8,customer.getCreatedDate());
-			else
-					throw new RARException("CustomerManager.save: can't save a Customer: Created Date undefined");
+			stmt.setLong(3,  hourlyPrice.getVehicleType().getId());
 			
-			if(customer.getMemberUntil() != null)
-				stmt.setDate(9,customer.getMemberUntil());
-			else
-				throw new RARException("CustomerManager.save: can't save a Customer: Member Until undefined");
-		
-			if(customer.getLicenseState() != null)
-				stmt.setString(10,customer.getLicenseState());
-			else
-				throw new RARException("CustomerManager.save: can't save a Customer: License State undefined");
-			
-			if(customer.getLicenseNumber() != null)
-				stmt.setString(11,customer.getLicenseNumber());
-			else
-				throw new RARException("CustomerManager.save: can't save a Customer: License Number undefined");
-			
-			if(customer.getCreditCardNumber() != null)
-				stmt.setString(12,customer.getCreditCardNumber());
-			else
-				throw new RARException("CustomerManager.save: can't save a Customer: Credit Card Number undefined");
-
-			if(customer.getCreditCardExpiration() != null)
-				stmt.setDate(13,customer.getCreditCardExpiration());
-			else
-				throw new RARException("CustomerManager.save: can't save a Customer: Credit Card Expiration undefined");
-
-			if(customer.getUserStatus() != null)
-				stmt.setString(14,customer.getUserStatus().toString());
-			else
-				throw new RARException("CustomerManager.save: can't save a Customer: User Status undefined");
-			
-			if(customer.isPersistent())
-				stmt.setLong(15,  customer.getId());
+			if(hourlyPrice.isPersistent())
+				stmt.setLong(3,  hourlyPrice.getId());
 		
 			inscnt = stmt.executeUpdate();
 			
-			if(!customer.isPersistent()) {
+			if(!hourlyPrice.isPersistent()) {
 				if(inscnt == 1) {
 					String sql = "select last_insert_id()";
 					if(stmt.execute(sql)) {
 						//retrieve the result
 						ResultSet r =stmt.getResultSet();
 						while(r.next()) {
-							userId = r.getLong(1);
-							if(userId > 0)
-								customer.setId(userId);
+							hourlyPriceId = r.getLong(1);
+							if(hourlyPriceId > 0)
+								hourlyPrice.setId(hourlyPriceId);
 						}//while
 					}//if
 				}//if
 			}else {
 				if(inscnt < 1)
-					throw new RARException("CustomerManager.save: failed to save a Customer");
+					throw new RARException("HourlyPriceManager.save: failed to save a hourly price");
 			}//if else
 			
 		}catch (SQLException e) {
 
 			e.printStackTrace();
-				throw new RARException("CustomerManager.save: Failed to save a Customer: " + e);
+				throw new RARException("HourlyPriceManager.save: Failed to save a Hourly Price: " + e);
 		}//try catch
 	}//store
 	
