0c6ad5c13358995cc5b6f3a42c995573d49c2c39
Jambrose777 <Jaa74458@uga.edu>
2017-10-23 21:31:29 -0400

    fixed back

--- src/edu/uga/cs/rentaride/persistence/impl/AdministratorManager.java
+++ src/edu/uga/cs/rentaride/persistence/impl/AdministratorManager.java
@@ -255,7 +255,6 @@ public class AdministratorManager {
 		            e.printStackTrace();
 		            throw new RARException( "AdministratorManager.delete: failed to delete a Administrator: " + e );       
 		            }
-		        catch(Exception e) { System.out.println("IT HAPPENED");};
 		    }
 		
 		
